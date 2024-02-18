0474f0f0163fbc5b98d1293cbcce8107c59bb9e2
John Lancaster <lancasjo@umich.edu>
2024-02-18 15:59:32 -0500

    implemented potholes

--- yolov8.py
+++ yolov8.py
@@ -3,8 +3,10 @@ from ultralytics import YOLO
 import numpy as np
 import sys
 
-def predict(video_path, model_name):
-    model = YOLO(model_name) # specify the model you would like to use
+def predict(video_path, lane_model, hole_model=None):
+
+    lane_model = YOLO(lane_model) # specify the model you would like to use
+    hole_model = YOLO(hole_model) if hole_model is not None else None
 
     video_path = video_path #make 0 if using webcam
     cap = cv2.VideoCapture(video_path)
@@ -14,60 +16,65 @@ def predict(video_path, model_name):
         success, frame = cap.read()
         
         if success:
-            results = model(frame) # this makes a prediction on a single frame of video
-            
-            annotated_frame = results[0].plot() # 
+            lane_results = lane_model(frame) # this makes a prediction on a single frame of video
+            hole_results = hole_model(frame) if hole_model is not None else None
+            lane_annotated_frame = lane_results[0].plot() # 
+            hole_annotated_frame = hole_results[0].plot() if hole_model is not None else None
+
             
             # for results in results:
             #     masks = results.masks  # masks gives us the coordinates for drivable area
             #     print(masks)
             #     break
-            image_height = annotated_frame.shape[0]
-            image_width = annotated_frame.shape[1]
-            occupancy_grid = np.zeros((image_height, image_width))
-            for r in results:
-                if(r.masks is not None):
+            image_height = lane_annotated_frame.shape[0]
+            image_width = lane_annotated_frame.shape[1]
+            occupancy_grid = np.zeros((image_height, image_width, 3))
+            for r in lane_results:
+                if r.masks is not None:
                     for segment in r.masks.xy:
                         segment_array = np.array([segment], dtype=np.int32)
                         cv2.fillPoly(occupancy_grid, [segment_array], color=(255, 255, 255))
-                        # for pixel in segment:
+            for r in hole_results:
+                if r.masks is not None:
+                    for segment in r.masks.xy:
+                        segment_array = np.array([segment], dtype=np.int32)
+                        cv2.fillPoly(occupancy_grid, [segment_array], color=(0, 0, 0))
+
+
                             
-                        #     x = pixel[0].astype(np.uint16)
-                        #     y = pixel[1].astype(np.uint16)
-                            
-                        #     # Ensure the coordinates are within the image dimensions
-                        #     if 0 <= x < image_width and 0 <= y < image_height:
-                        #         occupancy_grid[y, x] = 1
-                            
-            cv2.imshow("Occupancy Grid", occupancy_grid)
-            cv2.imshow("YOLOv8 Inference", annotated_frame)
+            cv2.imshow("Lane Lines", occupancy_grid)
+            # if hole_model is not None:
+            #     cv2.imshow("Potholes", hole_annotated_frame)
             
-            # if cv2.waitKey(1) & 0xFF == ord("q"): # press q to quit the program
-            #     break
-            
+            if cv2.waitKey(1) & 0xFF == ord("q"): # press q to quit the program
+                break
         else:
             break
 
     ##########################
     # Must have these dont touch
+    ##########################
     cap.release()
     cv2.destroyAllWindows()
-    ##########################
+
 
 if __name__ == "__main__":
     # Check if the script is run as the main program
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         print("Not enough parameters!! Please enter python3 yolov8.py <video_path> <model_name>")
         sys.exit(1)
 
     # Extract the command line argument (parameter)
     parameter_value = sys.argv[1]
     model_name = sys.argv[2]
+    hole_model = None
+    if len(sys.argv) == 4:
+        hole_model = sys.argv[3]
     if(parameter_value == "0"):
         parameter_value = int(parameter_value)
 
     # Call your function with the provided parameter
-    predict(parameter_value, model_name)
+    predict(parameter_value, model_name, hole_model)
 
 
 
