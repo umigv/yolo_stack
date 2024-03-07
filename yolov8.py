8ce5e514af0e9144ccfed7ec631a79cc912a477c
Matthew Gawthrop <mgawthro@umich.edu>
2024-03-07 13:24:53 -0500

    Better UX

--- yolov8.py
+++ yolov8.py
@@ -5,6 +5,9 @@ import sys
 import matplotlib.pyplot as plt
 import json
 
+
+
+
 def predict(video_path, lane_model, hole_model=None):
 
     lane_model = YOLO(lane_model) # specify the model you would like to use
@@ -45,6 +48,9 @@ def predict(video_path, lane_model, hole_model=None):
 
             cv2.imshow("Lane Lines", occupancy_grid)
             cv2.imshow("YOLOv8 Inference", lane_annotated_frame)
+            
+            
+            
             ##################For Nav Output not necessary for running
             # summed_grid = np.sum(occupancy_grid, axis=2)
 
@@ -56,6 +62,7 @@ def predict(video_path, lane_model, hole_model=None):
             # plt.show()
             # if hole_model is not None:
             #     cv2.imshow("Potholes", hole_annotated_frame)
+            
             
             if cv2.waitKey(1) & 0xFF == ord("q"): # press q to quit the program
                 break
@@ -72,13 +79,18 @@ def predict(video_path, lane_model, hole_model=None):
 if __name__ == "__main__":
     # Check if the script is run as the main program
     if len(sys.argv) < 3:
-        print("Not enough parameters!! Please enter python3 yolov8.py <video_path> <model_name>")
+        print("Not enough parameters!! Please enter; ")
+        print("python3 yolov8.py <video_path> <lane_line_model_path> <pot_holes_model_path>")
+        print("or python3 yolov8.py <video_path> <lane_line_model_path>")
+        print("enter 0 for the <video_path> if you are using a webcam device")
         sys.exit(1)
 
     # Extract the command line argument (parameter)
     parameter_value = sys.argv[1]
     model_name = sys.argv[2]
     hole_model = None
+    
+    
     if len(sys.argv) == 4:
         hole_model = sys.argv[3]
     if(parameter_value == "0"):
@@ -86,62 +98,3 @@ if __name__ == "__main__":
 
     # Call your function with the provided parameter
     predict(parameter_value, model_name, hole_model)
-
-
-
-
-# from ultralytics import YOLO
-# import cv2
-# import math 
-# # start webcam
-# cap = cv2.VideoCapture("output2.mp4")
-# cap.set(3, 480)#cap.set(3, 640)
-# cap.set(4, 320)#cap.set(4, 480)
-
-# # model
-# model = YOLO("yolov8withpotholes.pt")
-
-
-# while True:
-#     success, img = cap.read()
-#     results = model(img)
-
-#     # coordinates
-#     for r in results:
-#         masks = r.masks
-
-#         print(masks)
-
-#         boxes = r.boxes
-
-#         for box in boxes:
-#             # bounding box
-#             x1, y1, x2, y2 = box.xyxy[0]
-#             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
-
-#             # put box in cam
-#             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-
-#             # confidence
-#             confidence = math.ceil((box.conf[0]*100))/100
-#             #print("Confidence --->",confidence)
-
-#             # class name
-#             cls = int(box.cls[0])
-#             #print("Class name -->", classNames[cls])
-
-#             # object details
-#             org = [x1, y1]
-#             font = cv2.FONT_HERSHEY_SIMPLEX
-#             fontScale = 1
-#             color = (255, 0, 0)
-#             thickness = 2
-
-#             #cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
-
-#     cv2.imshow('Video', img)
-#     if cv2.waitKey(1) == ord('q'):
-#         break
-
-# cap.release()
-# cv2.destroyAllWindows()
